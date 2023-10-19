ca1276e59f71f2047651930a69709c9f043d76fd
Tyler Amos <tamos5@jhu.edu>
2023-10-18 19:23:12 -0600

    movie dictionary update - working

--- src/read_nd2.py
+++ src/read_nd2.py
@@ -3,38 +3,40 @@ import tifffile
 import numpy as np
 
 def read_nd2(nd2_filename):
-    with nd2reader.Nd2(nd2_filename) as nd2_movie:
-        nF = len(nd2_movie)
-        nC = 3
-
-        movie_dict = {}
-
-        for frame_number, frame in enumerate(nd2_movie):
+    with nd2reader.ND2Reader(nd2_filename) as nd2_movie:
+        nd2_movie.iter_axes = 'v'
+        well_data = {}
+        for fov in range(nd2_movie.sizes['v']):
             frame_data = {}
+            nd2_movie.iter_axes = 't'
+            for f in range(nd2_movie.sizes['t']):
+                nd2_movie.iter_axes = 'c'
+                channel_data = {}
+                
+                for c in range(nd2_movie.sizes['c']):
+                    channel_data[c] = nd2_movie[c]
+                
+                frame_data[f] = channel_data
+            well_data[fov] = frame_data
+                
+        
+    return well_data
 
-            for channel_number in range(nC):
-                frame_data[channel_number] = np.array(frame[channel_number])
-            movie_dict[frame_number] = frame_data
-    
-    return movie_dict
+def get_channel_rawData(well_data, site, frame, channel):
+    return well_data[site][frame][channel]
 
-def get_channel_rawData(movie_dict, frame, channel):
-    channel_matrix = []
-    if frame in movie_dict:
-        frame_data = movie_dict[frame]
-    
-        if channel in frame_data:
-            channel_matrix = frame_data[channel]
-        else:
-            print("Channel not found")
-    else:
-        print("frame not found")
+def get_frame_data(well_data, site, frame):
+    return well_data[site][frame]
 
-    return channel_matrix    
+def get_site_data(well_data, site):
+    return well_data[site]
 
 
-nd2_filename = "../doc/WellD01_ChannelmIFP,mCherry,YFP_Seq0000.nd2"
-test_movie = read_nd2(nd2_filename)
-f0_c0 = get_channel_rawData(test_movie, 0, 0)
-print(f0_c0)
-print(type(test_movie[0][0]))
+# nd2_filename = "../doc/WellD01_ChannelmIFP,mCherry,YFP_Seq0000.nd2"
+# well_data = read_nd2(nd2_filename)
+# site = 1
+# frame = 5
+# channel = 2
+# print(len(get_frame_data(well_data, site, frame)))
+# print(len(get_site_data(well_data, site)))
+# print(len(well_data))
