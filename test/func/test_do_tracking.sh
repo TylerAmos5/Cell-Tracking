f3a0f59481a6797a95a825f9be2411a5eab5a444
kbardsle <kaba6117@colorado.edu>
2023-12-13 16:19:22 -0700

    update functional tests

--- test/func/test_do_tracking.sh
+++ test/func/test_do_tracking.sh
@@ -1,6 +1,8 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_do_tracking python src/do_tracking.py --file_path "doc/WellD01_ChannelmIFP,mCherry,YFP_Seq0000.nd2"
-assert_equal $"WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv" $( ls $"WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv")
+run test_do_tracking python src/do_tracking.py --file_path "doc/WellD01_ChannelmIFP,mCherry,YFP_Seq0000.nd2" --output_path="output"
+assert_equal $"output/WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv" $( ls $"output/WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv")
+assert_equal $"output/channel2_plots/Cell_*.png" $( ls $"output/channel2_plots/Cell_*.png")
+assert_equal $"output/channel3_plots/Cell_*.png" $( ls $"output/channel3_plots/Cell_*.png")
 assert_exit_code 0
