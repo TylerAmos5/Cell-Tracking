fea2a94e1abe69964d25ad1da2787e1bbb7e59f6
Tyler Amos <tamos5@jhu.edu>
2023-12-13 17:23:46 -0700

    CI bug fixes

--- test/func/test_do_tracking.sh
+++ test/func/test_do_tracking.sh
@@ -3,6 +3,6 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 run test_do_tracking python src/do_tracking.py --file_path "doc/WellD01_ChannelmIFP,mCherry,YFP_Seq0000.nd2" --output_path="output"
 assert_equal $"output/WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv" $( ls $"output/WellD01_ChannelmIFP,mCherry,YFP_Seq0000_tracks.csv")
-assert_equal $"channel2_plots" $( ls $"output")
+assert_equal $"channel2_plots" $( ls $"output" | grep "channel2")
 assert_equal $"Cell_0.png" $( ls $"output/channel2_plots")
 assert_exit_code 0
